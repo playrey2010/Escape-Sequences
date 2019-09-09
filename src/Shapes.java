4dea3d14f7a19873f1e58796113596e19943d3ae
playrey2010 <playrey2010@gmail.com>
2019-09-09 14:14:39 -0400

    finished but adding print f solutions later

---
+++ src/Shapes.java
@@ -0,0 +1,27 @@
+public class Shapes {
+    public static void main(String[] args) {
+        System.out.println("*********\n" +
+                "*\t\t*\n" +
+                "*\t\t*\n" +
+                "*\t\t*\n" +
+                "*\t\t*\n" +
+                "*********");
+        System.out.println("********\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "*      *\n" +
+                "********");
+        System.out.println("  ***  \n" +
+                " *   *\n" +
+                "*     *\n" +
+                "*     *\n" +
+                " *   *\n" +
+                "  ***\n");
+        System.out.println("   *\n" +
+                "  * *\n" +
+                " *   *\n" +
+                "  * *\n" +
+                "   *");
+    }
+}
